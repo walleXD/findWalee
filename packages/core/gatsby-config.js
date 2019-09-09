5574cf383696b5b050a92f56525141efff059248
Walee Ahmed <dev@walee.me>
2019-09-08 22:46:41 -0400

    initial commit with basic gatsby setup

---
+++ packages/core/gatsby-config.js
@@ -0,0 +1,9 @@
+const plugins = [
+  `gatsby-plugin-typescript`,
+  `gatsby-plugin-material-ui`,
+  "gatsby-plugin-top-layout"
+]
+
+module.exports = {
+  plugins,
+}
---
+++ packages/core/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from "react"
+import { Typography, Box } from "@material-ui/core"
+
+const LandingPage = (): ReactElement => (
+  <Box>
+    <Typography>Hello world!</Typography>
+  </Box>
+)
+
+export default LandingPage
